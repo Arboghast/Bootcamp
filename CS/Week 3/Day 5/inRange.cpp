8b36c781b643988e40e7b756b29c1d879bfa46dd
Sami <BlueBubbleBeam@outlook.com>
2019-08-16 14:15:57 -0400

    uncommited hw problems from leetcode

---
+++ CS/Week 3/Day 5/inRange.cpp
@@ -0,0 +1,27 @@
+class RecentCounter {
+public:
+    int start= 0;
+    int end=0;
+    vector<int> counter; 
+    RecentCounter() {
+        counter = vector<int>(0);
+    }
+    
+    int ping(int t) {
+        if(counter.size() != 0)
+        {
+            end++;//bugged
+        }
+        counter.push_back(t);
+        while(counter[end]-counter[start] > 3000){
+            start++;
+        }
+        return end-start+1;
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
